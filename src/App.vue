fd4dadc61948530ae9544217f805e58bcc9d3ede
wade2006 <1121157674@qq.com>
2020-12-21 15:36:29 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0px;
+  padding: 0px;
+}
+@font-face {
+  font-family: 'iconfont';  /* project id 2282833 */
+  src: url('//at.alicdn.com/t/font_2282833_iybi3vm4b68.eot');
+  src: url('//at.alicdn.com/t/font_2282833_iybi3vm4b68.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_2282833_iybi3vm4b68.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_2282833_iybi3vm4b68.woff') format('woff'),
+  url('//at.alicdn.com/t/font_2282833_iybi3vm4b68.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_2282833_iybi3vm4b68.svg#iconfont') format('svg');
+}
+.iconfont{
+  font-family: "iconfont";
+  font-size: 16px;
+  font-style: normal;
 }
 </style>
